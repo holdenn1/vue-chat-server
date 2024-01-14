f32b03c57ba1578213d900259b66fb908ef80520
holdenn1 <caulfieldd17@gmail.com>
2024-01-14 14:41:00 +0200

    small fixes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -19,10 +19,12 @@ export class AuthController {
     res.cookie('access_token', accessToken, {
       httpOnly: true,
       secure: false,
+      sameSite: 'none',
       expires: new Date(new Date().getTime() + 15 * 60 * 1000),
     });
     res.cookie('refresh_token', refreshToken, {
       httpOnly: true,
+      sameSite: 'none',
       secure: false,
       expires: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
     });
@@ -65,11 +67,13 @@ export class AuthController {
 
     res.clearCookie('access_token', {
       httpOnly: true,
+      sameSite: 'none',
       secure: false,
     });
 
     res.clearCookie('refresh_token', {
       httpOnly: true,
+      sameSite: 'none',
       secure: false,
     });
 
